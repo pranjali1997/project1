fc083359ff3d85b1dcf2a77086c58746bca28de5
pranjali1997 <pranjalirpawar@gmail.com>
2023-03-19 17:15:38 +0530

    2nd commit

---
+++ ppp/FirstQuadrant.java
@@ -0,0 +1,19 @@
+package starPattern;
+
+public class FirstQuadrant {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int j;
+		
+		for(i=1; i<=5;i++) 
+		{
+			for(j=5; j>=i; j--) 
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+}
